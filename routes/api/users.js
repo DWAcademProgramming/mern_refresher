ebacdb953004799454156988c9e50e37b94a95de
DWAcademProgramming <95488893+DWAcademProgramming@users.noreply.github.com>
2022-01-02 10:14:20 -0600

    Add files via upload

--- routes/api/users.js
+++ routes/api/users.js
@@ -3,6 +3,8 @@ const res = require('express/lib/response');
 const router = express.Router(); 
 const gravatar = require('gravatar'); 
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken'); 
+const config = require('config'); 
 const { check, validationResult } = require('express-validator'); 
 
 // Get the Model
@@ -55,7 +57,21 @@ async (req, res) => {
         user.password = await bcrypt.hash(password, salt); 
         await user.save();
         // Return jsonwebtoken
-        res.send('User Register'); 
+        const payload = {
+            user: {
+                id: user.id
+            }
+        }
+
+        jwt.sign(
+            payload, 
+            config.get('jwtSecret'), 
+            { expiresIn: 360000}, 
+            (err, token) => {
+                if (err) throw err; 
+                res.json({ token })
+            }
+        ); 
 
     } catch(err){
         res.status(500).send('Server error')
